e824d8d1897e00e8a3dbb61fb13b7481e00529a6
andres <aebb001@gmail.com>
2020-08-14 18:09:50 -0400

    Updates

---
+++ precache-manifest.922a42bddb6dbe7a45e6340663c4e25b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a267a903d0e7c114cb0079cb4bac1014",
+    "url": "/goldenState-team/index.html"
+  },
+  {
+    "revision": "591e344c99b617cc5e8a",
+    "url": "/goldenState-team/static/css/main.0d52d90a.chunk.css"
+  },
+  {
+    "revision": "91658e3e1f51a0d93b8d",
+    "url": "/goldenState-team/static/js/2.3d3e4b4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goldenState-team/static/js/2.3d3e4b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591e344c99b617cc5e8a",
+    "url": "/goldenState-team/static/js/main.cbbe3cad.chunk.js"
+  },
+  {
+    "revision": "44bdda9a4bf96ca9700b",
+    "url": "/goldenState-team/static/js/runtime-main.e15fc9dc.js"
+  },
+  {
+    "revision": "09360ee0c192f1e4efb9deadb4616f11",
+    "url": "/goldenState-team/static/media/fieldOriginal.09360ee0.jpg"
+  }
+]);
